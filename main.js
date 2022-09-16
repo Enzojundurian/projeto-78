56fdcb2a47833fe8c8a51235713d4b53594e8734
Enzojundurian <104839520+Enzojundurian@users.noreply.github.com>
2022-09-16 11:06:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("meu primeiro projeto no console");
+var nome="Enzoca ";
+var sobrenome="Jundurians";
+var ns= nome.concat(sobrenome);
+console.log(ns);
+var a=10;
+var b=6;
+var ab=a+b;
+console.log(ab);
+
+
+
+
+
+
